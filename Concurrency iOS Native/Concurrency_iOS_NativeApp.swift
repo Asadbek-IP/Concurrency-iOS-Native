13113bbaabeb279a478bffcaf30a6adfc9a30c2a
asadbek-ip <dasturchi6678@gmail.com>
2023-08-25 15:17:26 +0500

    Initial Commit

---
+++ Concurrency iOS Native/Concurrency_iOS_NativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrency_iOS_NativeApp.swift
+//  Concurrency iOS Native
+//
+//  Created by Mac Mini 1 on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Concurrency_iOS_NativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
